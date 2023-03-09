36a30621cf2ae68be5506a8cbb00c42cbf84e732
boryand <boryand>
2023-03-09 18:28:10 +0100

    Adding the routing system and the basic content of the website

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      name: "PEONY, ROSE & LILY",
+      slug: "peony, rose & lily",
+      category: "Flowers",
+      image:
+        "https://media.bloomandwild.com/v1/1000x1000/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/jo-ht-/c98ed85d-22c1-4778-9d8c-6eecf67d3fcf.jpg",
+      price: 35,
+      rating: 4.5,
+      numReviews: 80,
+      description:
+        " The seasonal favourite peony has been perfectly paired with the pink oriental lily and white avalanche roses for a stunning summertime gift.",
+    },
+    {
+      name: "PEACHES & PEONY",
+      slug: "Peaches and Peony",
+      category: "Flowers",
+      image:
+        "https://media.bloomandwild.com/v1/1000x1000/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/mareike-ht-de/08b6b797-0c23-4081-b8ff-b7a94f5abb30.jpg",
+      price: 55,
+      rating: 4.8,
+      numReviews: 70,
+      description:
+        " Surprise them this summer with this beautiful bouquet of creamy white peonies, soft peach roses, and salmon lilies.",
+    },
+    {
+      name: "DAIQUIRI",
+      slug: "daiquiri",
+      category: "Flowers",
+      image:
+        "https://media.bloomandwild.com/v1/1080x1080/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/lago-di-como-v2/bde043ab-d2ff-4f54-9966-c3245587b6cb.jpg",
+      price: 70,
+      rating: 4.5,
+      numReviews: 50,
+      description:
+        " Send the scent of summer with this summertime favourite of pink hydrangea, cerise roses with textured burgundy carnations and green hypericum berries.",
+    },
+    {
+      name: "GLAZE",
+      slug: "Glaze",
+      category: "Flowers",
+      image:
+        "https://media.bloomandwild.com/v1/1080x1080/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/floristen-highlight-a33f2c3e-2e84-4b3f-9f8d-9c70d77eaceb/a43f19a6-7728-4e39-9aa4-ee6ce66f4c44.jpg",
+      price: 35,
+      rating: 4.5,
+      numReviews: 60,
+      description:
+        " Pure white fragrant oriental lilies with vibrant yellow roses, sugar flair hypericum berries and alluring lilac memory lane roses.",
+    },
+    {
+      name: "Purple Dream",
+      slug: "Purple Flowers",
+      category: "Flowers",
+      image:
+        "https://www.floraprima.de/media/cache/ZmFydGlrZWwvNzkzMTgvOTY3M18xXzFfN18zMl8xMzBfY3hfZXBuZ19jeF93OTAwX2N4X2g5MDBfY3hfbTE2NzY0NjE2NDZfY3hfZDA=/9673-frisch-gebunden.jpg",
+      price: 35,
+      rating: 4.5,
+      numReviews: 60,
+      description:
+        " Pure white fragrant oriental lilies with vibrant yellow roses, sugar flair hypericum berries and alluring lilac memory lane roses.",
+    },
+    {
+      name: "Romeo Flowers",
+      slug: "Romeo Flowers",
+      category: "Flowers",
+      image:
+        "https://www.floraprima.de/media/cache/ZmFydGlrZWwvNTcyNTAvNjA5NV8xXzFfN18zMl8xMzBfY3hfZXBuZ19jeF93OTAwX2N4X2g5MDBfY3hfbTE2Nzc2NTUxOTNfY3hfZDM=/6095-frisch-gebunden.jpg",
+      price: 35,
+      rating: 4.5,
+      numReviews: 60,
+      description:
+        " Pure white fragrant oriental lilies with vibrant yellow roses, sugar flair hypericum berries and alluring lilac memory lane roses.",
+    },
+  ],
+};
+
+export default data;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import data from "../data.js";
+import { Link } from "react-router-dom";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img id="img1" src={product.image} alt={product.name} />{" "}
+            </Link>
+            <div className="product-info">
+              {" "}
+              <Link to={`/product/${product.slug}`}>
+                {" "}
+                <p>{product.name}</p>{" "}
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
