00e7f5e4b915dfb69308a0fcc7478fea90774e70
boryand <boryand>
2023-03-17 14:20:56 +0100

    some bug fixes with MOngoDv

--- backend/data.js
+++ backend/data.js
@@ -2,7 +2,7 @@ const data = {
   products: [
     {
       // _id: "1",
-      name: "Peony, Rose & Lily",
+      name: " Rose & Lily",
       slug: "peony",
       category: "Flowers",
       image:
@@ -30,7 +30,7 @@ const data = {
       // _id: "3",
       name: "Daiquiri",
       slug: "daiquiri",
-      category: "Flowers",
+      category: "Gifts",
       image:
         "https://media.bloomandwild.com/v1/1080x1080/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/lago-di-como-v2/bde043ab-d2ff-4f54-9966-c3245587b6cb.jpg",
       price: 70,
@@ -43,7 +43,7 @@ const data = {
       // _id: "4",
       name: "Glaze",
       slug: "Glaze",
-      category: "Flowers",
+      category: "Gifts",
       image:
         "https://media.bloomandwild.com/v1/1080x1080/filters:format(webp):quality(75)/https://assets-0.bloomandwild.com/letterbox-main/floristen-highlight-a33f2c3e-2e84-4b3f-9f8d-9c70d77eaceb/a43f19a6-7728-4e39-9aa4-ee6ce66f4c44.jpg",
       price: 35,
@@ -51,58 +51,6 @@ const data = {
       numReviews: 60,
       description:
         " Pure white fragrant oriental lilies with vibrant yellow roses, sugar flair hypericum berries and alluring lilac memory lane roses.",
-    },
-    {
-      // _id: "5",
-      name: "Red Rose bear",
-      slug: "rose-bear",
-      category: "Gifts",
-      image:
-        "https://cdn.shopify.com/s/files/1/0589/1280/0936/products/miszsercemczerwony_4e4c06b1-6789-4fdd-97e6-ac0f15d01806_600x.png?v=1677147194",
-      price: 35,
-      rating: 4.5,
-      numReviews: 60,
-      description:
-        " Rose bear with personalized glass print and with your photo",
-    },
-    {
-      // _id: "6",
-      name: "Teddy Bear",
-      slug: "black-bear",
-      category: "Gifts",
-      image:
-        "https://cdn.shopify.com/s/files/1/0589/1280/0936/products/228_7_600x.jpg?v=1663925010",
-      price: 35,
-      rating: 4.5,
-      numReviews: 60,
-      description:
-        " Teddy Bear with Diamonds | Black | Gift for women, girlfriend, mom, Valentine's Day, birthday, Christmas gifts, Mother's Day gifts, International Women's Day",
-    },
-    {
-      // _id: "7",
-      name: "Red eternal rose ",
-      slug: "led-rose",
-      category: "Gifts",
-      image:
-        "https://cdn.shopify.com/s/files/1/0589/1280/0936/products/IMG_6152_1_4196ec04-40c3-415f-93ae-cea7ea5504b2_600x.jpg?v=1644406923",
-      price: 65,
-      rating: 4.5,
-      numReviews: 80,
-      description:
-        " Red eternal rose in a glass LED, gift for women, girlfriend, mum, Valentine's Day, birthday, Christmas gifts, Mother's Day gifts, International Women's Day",
-    },
-    {
-      // _id: "8",
-      name: "Rose bear ",
-      slug: "pink-rose-bear",
-      category: "Gifts",
-      image:
-        "https://cdn.shopify.com/s/files/1/0589/1280/0936/products/IMG_7136_600x.jpg?v=1647436056",
-      price: 155,
-      rating: 4.8,
-      numReviews: 70,
-      description:
-        " Rose bear pink with heart 60c, gift for women, girlfriend, mum, Valentine's Day, birthday, Christmas gifts, Mother's Day gifts, International Women's Day",
     },
   ],
 };
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import Product from '../models/productModel.js';
-import data from '../data.js';
+import express from "express";
+import Product from "../models/productModel.js";
+import data from "../data.js";
 
 const seedRouter = express.Router();
 
-seedRouter.get('/', async (req, res) => {
-  await Product.deleteOne({});
-  const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+seedRouter.get("/", async (req, res) => {
+  try {
+    await Product.deleteOne({});
+    const createdProducts = await Product.insertMany(data.products);
+    res.send({ createdProducts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error seeding products");
+  }
 });
+
 export default seedRouter;
