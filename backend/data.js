ca28d58979556eee4507cd118068bec4e334443b
boryand <boryand>
2023-04-05 14:54:16 +0200

    Created sample users and encrypted the passwords

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: "Bobby",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Max",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       // _id: "1",
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
 import Product from "../models/productModel.js";
 import data from "../data.js";
+import User from "../models/userModel.js";
 
 const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
-  try {
-    await Product.deleteOne({});
-    const createdProducts = await Product.insertMany(data.products);
-    res.send({ createdProducts });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error seeding products");
-  }
+  await Product.deleteMany({});
+  const createdProducts = await Product.insertMany(data.products);
+
+  await User.deleteMany({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRouter;
