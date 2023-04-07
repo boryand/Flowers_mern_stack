665caebed52ce9087af98775fbb2fdf608f1d0c7
boryand <boryand>
2023-04-07 13:07:30 +0200

    creating the select payment method step

--- src/Store.js
+++ src/Store.js
@@ -11,6 +11,9 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
     ? JSON.parse(localStorage.getItem('shippingAddress'))
     : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+    ? localStorage.getItem('paymentMethod')
+    : '',
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
@@ -57,7 +60,11 @@ function reducer(state, action) {
           shippingAddress: action.payload,
         },
       };
-
+      case 'SAVE_PAYMENT_METHOD':
+        return {
+          ...state,
+          cart: { ...state.cart, paymentMethod: action.payload },
+        };
     default:
       return state;
   }
